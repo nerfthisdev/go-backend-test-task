c18eb09e7ef89723397d22363b53e37b40ee8011
nerfthisdev <barm2001@gmail.com>
2025-07-02 01:31:02 +0300

    feat: add db initialization

---
+++ internal/repository/repo.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Repository struct {
+	DB *pgxpool.Pool
+}
+
+func Init(ctx context.Context) (*Repository, error) {
+	dburi := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	cfg, err := pgxpool.ParseConfig(dburi)
+	if err != nil {
+		return nil, err
+	}
+
+	dbpool, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Repository{
+		DB: dbpool,
+	}, nil
+}
+
+func (r *Repository) initSchema(ctx context.Context) error {
+	query := `CREATE TABLE IF NOT EXISTS refresh_tokens (
+			guid UUID NOT NULL,
+			token TEXT PRIMARY KEY,
+			expires_at TIMESTAMP NOT NULL
+		);`
+
+	_, err := r.DB.Exec(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
