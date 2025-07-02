657f296daef7334538045d81fe639fa90a6a102f
nerfthisdev <barm2001@gmail.com>
2025-07-02 12:33:14 +0300

    feat: better context logic and db initialization

--- cmd/main.go
+++ cmd/main.go
@@ -5,41 +5,45 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/nerfthisdev/go-backend-test-task/internal/repository"
 )
 
+const defaultTimeout = 5 * time.Second
+
 func main() {
-	err := godotenv.Load()
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Fatal(err.Error())
 	}
 
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	appCtx := context.Background()
+
+	ctx, cancel := context.WithTimeout(appCtx, defaultTimeout)
 	defer cancel()
 
 	repo, err := repository.Init(ctx)
 	if err != nil {
 		log.Fatalf("failed to init repository: %v", err.Error())
 	}
-
 	defer repo.DB.Close()
-
 	log.Println("successfully connected to db")
+
+	if err = repo.InitSchema(ctx); err != nil {
+		log.Fatalf("failed to init table: %v", err.Error())
+	}
 
 	router := http.NewServeMux()
 
 	port := ":" + os.Getenv("HTTP_PORT")
-
 	server := http.Server{
 		Addr:    port,
 		Handler: router,
 	}
 
-	err = server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("failed to start http server: %v", err.Error())
+	log.Printf("starting server on %s", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("failed to start server: %v", err.Error())
 	}
 }
