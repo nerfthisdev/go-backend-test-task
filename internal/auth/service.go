3fcde83043732a383f1c9dc0ab1c0df13ad512b5
nerfthisdev <barm2001@gmail.com>
2025-07-02 13:10:11 +0300

    feat: add access and refresh token creation

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,9 +1,14 @@
 package auth
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"github.com/nerfthisdev/go-backend-test-task/internal/repository"
 )
 
@@ -23,4 +28,30 @@ func NewAuthService(repo *repository.Repository, signingKey string, accessTTL, r
 		accessTTL:  accessTTL,
 		refreshTTL: refreshTTL,
 	}
+}
+
+func (s *AuthService) CreateTokenPair(ctx context.Context, guid uuid.UUID) (TokenPair, error) {
+	// HMAC and SHA512
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.RegisteredClaims{
+		Subject:   guid.String(),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(s.accessTTL)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	})
+
+	accessTokenString, err := accessToken.SignedString(s.signingKey)
+	if err != nil {
+		return TokenPair{}, err
+	}
+
+	refreshTokenBytes := make([]byte, 32)
+	if _, err := rand.Read(refreshTokenBytes); err != nil {
+		return TokenPair{}, err
+	}
+
+	refreshTokenString := base64.StdEncoding.EncodeToString(refreshTokenBytes)
+
+	return TokenPair{
+		AccessToken:  accessTokenString,
+		RefreshToken: refreshTokenString,
+	}, err
 }
